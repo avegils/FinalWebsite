a1e42aa42da84e62089beb2e7864f35f7a2407b2
avegils <114716886+avegils@users.noreply.github.com>
2023-05-02 18:42:12 -0400

    Initial commit

---
+++ enabler.js
@@ -0,0 +1,6 @@
+// Javascript that enables popover buttons to display text when clicked
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
